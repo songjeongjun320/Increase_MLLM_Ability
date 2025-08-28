cceae20147345cc20312d7b36846f974f1970b31
[ Jeongjun Song - ENGR Academic Jsong132 Student Affrs - Undergraduate - jsong132 ] <songjeongjun320@gmail.com>
2025-08-27 21:16:06 -0700

    evaluation, tow training

--- 3_evaluation/MMLU/eval_mmlu_5shot.py
+++ 3_evaluation/MMLU/eval_mmlu_5shot.py
@@ -102,7 +102,7 @@ MODEL_CONFIGS = [
 
 # --- General Configuration ---
 DATASET_PATH = "../../2_datasets/MMLU/MMLU_origin.json"
-BASE_OUTPUT_DIR = "mmlu_5shot" # 5-shot evaluation results
+BASE_OUTPUT_DIR = "mmlu_5shot_results" # 5-shot evaluation results
 BATCH_SIZE = 16
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 CACHE_DIR = "./cache" if not os.path.exists("/scratch/jsong132/.cache/huggingface") else "/scratch/jsong132/.cache/huggingface"

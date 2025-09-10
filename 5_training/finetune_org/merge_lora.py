7a66ecd3b018ac0d951324758890e12418083855
[ Jeongjun Song - ENGR Academic Jsong132 Student Affrs - Undergraduate - jsong132 ] <songjeongjun320@gmail.com>
2025-09-10 01:05:56 -0700

    merge

--- 5_training/finetune_org/merge_lora.py
+++ 5_training/finetune_org/merge_lora.py
@@ -8,7 +8,7 @@ from safetensors import safe_open
 
 # --- 설정 (본인 환경에 맞게 수정하세요) ---
 base_model_path = "/scratch/jsong132/Increase_MLLM_Ability/Base_Models/llama-3.2-3b-pt"
-adapter_path = "./tow_trained_models/llama-3.2-3b-pt-tow-09_05_allenai"
+adapter_path = "./scratch/jsong132/Increase_MLLM_Ability/5_training/finetune_org/tow_trained_models/llama-3.2-3b-pt-tow-09_05_allenai"
 output_merged_model_path = "./merged_models/llama-tow-allenai-merged"
 # -----------------------------------------
 

7d279ad5435717286eac76f523b4f10cc0934767
[ Jeongjun Song - ENGR Academic Jsong132 Student Affrs - Undergraduate - jsong132 ] <songjeongjun320@gmail.com>
2025-09-21 00:52:52 -0700

    olmo

--- 3_evaluation/ARC/eval_arc_3shot.py
+++ 3_evaluation/ARC/eval_arc_3shot.py
@@ -56,11 +56,11 @@ MODEL_CONFIGS = [
     #     model_id="/scratch/jsong132/Increase_MLLM_Ability/Base_Models/gemma-3-4b-pt",
     #     use_quantization=False
     # ),
-    # ModelConfig(
-    #     name="olmo-2-0425-1b",
-    #     model_id="/scratch/jsong132/Increase_MLLM_Ability/Base_Models/olmo-2-0425-1b",
-    #     use_quantization=False
-    # ),
+    ModelConfig(
+        name="olmo-2-0425-1b",
+        model_id="/scratch/jsong132/Increase_MLLM_Ability/Base_Models/olmo-2-0425-1b",
+        use_quantization=False
+    ),
 
     # ModelConfig(
     #     name="llama-3.2-3b-pt-tow-09_11_2epoch_allenai-merged",
@@ -98,11 +98,11 @@ MODEL_CONFIGS = [
     #     model_id="/scratch/jsong132/Increase_MLLM_Ability/5_training/finetune_org/merged_models/gemma-3-4b-pt-tow-09_11_2epoch_org_initialize-merged",
     #     use_quantization=False
     # ),
-    ModelConfig(
-        name="olmo-2-0425-1b-tow-09_11_2epoch_org_initialize-merged",
-        model_id="/scratch/jsong132/Increase_MLLM_Ability/5_training/finetune_org/merged_models/olmo-2-0425-1b-tow-09_11_2epoch_org_initialize-merged",
-        use_quantization=False
-    ),
+    # ModelConfig(
+    #     name="olmo-2-0425-1b-tow-09_11_2epoch_org_initialize-merged",
+    #     model_id="/scratch/jsong132/Increase_MLLM_Ability/5_training/finetune_org/merged_models/olmo-2-0425-1b-tow-09_11_2epoch_org_initialize-merged",
+    #     use_quantization=False
+    # ),
 
     # ModelConfig(
     #     name="llama-3.2-3b-tow-09_11_2epoch_fix_tow-merged",
